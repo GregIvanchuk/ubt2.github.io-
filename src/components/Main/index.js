f15ef8636e9e06e7e62f0d88bde2eef68fb816bd
Greg Ivanchuk <gregiv99gmail.com>
2023-04-06 22:49:20 +0300

    add component HowWEWork

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -9,6 +9,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Card from "../Card";
 import ForUs from "./ForUs";
+import HowWEWork from "./HowWeWork";
 
 // import testSlider from "./testSlider";
 function Main({ parentData }) {
@@ -36,10 +37,10 @@ function Main({ parentData }) {
                     <MainSlider />
                 </section>
                 <div className="container">
-                    <div className={styles.yellowBlock}>
+                    {/* <div className={styles.yellowBlock}>
                         <h1>ЗРУЧНІ ТА ДОСТУПНІ ПРОЕКТИ ДИЗАЙНУ ІНТЕР’ЄРУ</h1>
                         <button className={styles.detail}>Детальніше</button>
-                    </div>
+                    </div> */}
                     {/* <testSlider/> */}
                     <section id="services"><Services /></section>
                   
@@ -47,6 +48,7 @@ function Main({ parentData }) {
                         <Card />
                     </section>
                     <section id="about">
+                      <HowWEWork/>
                       <ForUs />
                     <YellowBlock />
                     {/* <SecondSlider />  */}
