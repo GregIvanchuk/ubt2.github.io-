510b11bb2346864a28058b3312e367e52949ec65
GregIvanchuk <gregiv99@gmail.com>
2023-05-22 10:54:53 +0300

    add conect button

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ import Pr5 from "./components/Main/Pr5";
 import Pr6 from "./components/Main/Pr6";
 
 function App() {
+    const [isClicked , setIsClicked] = useState(false);
+    const clickHandler = () => {
+        setIsClicked(!isClicked);
+    }
     const ScrollToTop = () => {
         const location = useLocation();
     
@@ -90,6 +94,21 @@ function App() {
                         <Route exact path="about" element={<ForUs />} />
                     </Routes>
                 </div>
+                { isClicked ?
+                  <div className={styles.Cont}>
+                  <div className={styles.items}>
+                  <a href="viber://chat?number=+380937725057"><img width={80} height={80} src="/images/viber.png" alt="viber" /></a>
+             <a  href="https://t.me/+380937725057" target="_blank" ><img width={80} height={80} src="/images/telegram.png" alt="telegram" /></a>
+                  </div>
+                  <div  onClick={clickHandler} className={styles.conect1}>
+                  <img height={50} width={50} src="/images/del.png" alt="chat" />
+                  </div>
+                 </div>
+                :
+                <div onClick={clickHandler}  className={styles.conect}>
+                <img height={50} width={50} src="/images/chat.png" alt="chat" />
+            </div>
+                }
                 <Footer />
             </Router>
         </>
